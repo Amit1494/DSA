57bf59cd5b664dde4c391cd8057bdecd7c6544f1
Amit1494 <146162073+Amit1494@users.noreply.github.com>
2026-09-19 14:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (51.08%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,6 +1,16 @@
 class Solution {
     public boolean divisorGame(int n) {
-        if(n%2==0)return true;
+        return helper(n);
+    }
+    public boolean helper(int n){
+        if(n<=0){
+            return false;
+        }
+        for(int x=1;x<n;x++){
+            if(n%x==0){
+                return !helper(n-x);
+            }
+        }
         return false;
     }
 }

c57edf6cd0069017b97be1543edeb1aa777e13d7
Amit1494 <146162073+Amit1494@users.noreply.github.com>
2026-09-26 17:09:33 +0530

    Time: 1 ms (99.78%) | Memory: 47.3 MB (57.55%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,24 +1,29 @@
 class Solution {
     public int jump(int[] nums) {
-        int dp[]=new int[nums.length+1];
-        
-            Arrays.fill(dp,-1);
-        
-        return helper(0,nums,dp);
-    }
-    public int helper(int ind,int arr[],int dp[]){
-
-        if(ind>=arr.length-1)return 0;
-        if(dp[ind]!=-1)return dp[ind];
-        int mini=Integer.MAX_VALUE;
-        for(int i=1;i<=arr[ind];i++){
-            if(ind+1<arr.length){
-                int jumps=helper(ind+i,arr,dp);
-                if(jumps!=Integer.MAX_VALUE){
-                    mini=Math.min(mini,1+jumps);
-                }
+        int jumps =0;
+        int end=0;
+        int farthest=0;
+        for(int i=0;i<nums.length-1;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            if(i==end){
+                jumps++;
+                end=farthest;
             }
-        }
-        return dp[ind]=mini;
+        }return jumps;
     }
+    // public int helper(int ind,int arr[],int dp[]){
+
+    //     if(ind>=arr.length-1)return 0;
+    //     if(dp[ind]!=-1)return dp[ind];
+    //     int mini=Integer.MAX_VALUE;
+    //     for(int i=1;i<=arr[ind];i++){
+    //         if(ind+1<arr.length){
+    //             int jumps=helper(ind+i,arr,dp);
+    //             if(jumps!=Integer.MAX_VALUE){
+    //                 mini=Math.min(mini,1+jumps);
+    //             }
+    //         }
+    //     }
+    //     return dp[ind]=mini;
+    // }
 }
